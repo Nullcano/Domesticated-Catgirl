a30057ad60e8979f16868245567219cfdcddbe82
Shimeri <mo@shi.moe>
2018-12-30 03:30:34 +0100

    New functions

--- js/script.js
+++ js/script.js
@@ -1,89 +1,115 @@
 function load() {
-    clock();
-    Img();
-    time();
+    greet();
+    calendar();
 }
 
-function clock(){
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    m = checkTime(m);
-    document.getElementById('clock').innerHTML =
-        h + ":" + m;
-    var t = setTimeout(Clock, 500);
+$(document).ready(function(){
+  function displayTime(){
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+
+    if (minutes < 10){
+      minutes = "0" + minutes;
+    }
+
+    var meridiem = "AM";
+    if (hours > 12){
+      hours = hours - 12;
+      meridiem = "PM";
+    }
+    if (hours < 10){
+      hours = "0" + hours;
+    }
+    if (hours === 0 ){
+      hours = 12;
+    }
+
+    var clockDiv = document.getElementById('clock');
+    clockDiv.innerText = hours + ":" + minutes + " " + meridiem;
+  }
+
+  displayTime();
+  setInterval(displayTime, 1000);
+});
+
+function greet(){
+  var today = new Date();
+  var h = today.getHours();
+  console.log(h);
+
+  if(h>=6 && h<=11){
+    document.getElementById('img').style.backgroundImage = "url('img/morning2.jpg')";
+    document.getElementById('greeting').innerHTML = "Good morning, Master!";
+  }
+  if(h>=12 && h<=16) {
+    document.getElementById('img').style.backgroundImage = "url('img/day1.jpg')";
+    document.getElementById('greeting').innerHTML = "Good day, Master!";
+  }
+  if(h>=17 && h<=19) {
+    document.getElementById('img').style.backgroundImage = "url('img/noon1.jpg')";
+    document.getElementById('greeting').innerHTML = "Good afternoon, Master!";
+  }
+  if((h>=20 && h<=23) || (h>=0 && h<=5)){
+    document.getElementById('img').style.backgroundImage = "url('img/night1.jpg')";
+    document.getElementById('greeting').innerHTML = "Good night, Master!";
+  }
 }
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};
-    return i;
+function calendar(){
+  var today = new Date();
+  var weekday = new Array(7);
+  weekday[0]=  "Sun";
+  weekday[1] = "Mon";
+  weekday[2] = "Tue";
+  weekday[3] = "Wed";
+  weekday[4] = "Thu";
+  weekday[5] = "Fri";
+  weekday[6] = "Sat";
+
+  var day = weekday[today.getDay()];
+  var d = today.getDate();
+  var month = new Array(12);
+  month[0] = "Jan";
+  month[1] = "Feb";
+  month[2] = "Mar";
+  month[3] = "Apr";
+  month[4] = "May";
+  month[5] = "Jun";
+  month[6] = "Jul";
+  month[7] = "Aug";
+  month[8] = "Sep";
+  month[9] = "Oct";
+  month[10] = "Nov";
+  month[11] = "Dec";
+
+  var m = month[today.getUTCMonth()];
+  var total = day + " " + d + " " + m;
+  console.log(total);
+
+  document.getElementById('date').innerHTML = total;
 }
 
-function Img(){
-    var today = new Date();
-    var h = today.getHours();
-    console.log(h);
-
-    if(h>=6 && h<=11){
-        //change the images ONLY IF YOU KNOW WHAT YOUR DOING   "url('www.yourimage.jpg')" OR change the images in the map and rename.
-        document.getElementById('img').style.backgroundImage = "url('img/morning2.jpg')";
-        // Change position ONLY IF YOU KNOW WHAY YOU ARE DOING     "-123px"
-        document.getElementById('img').style.backgroundPositionY = "-210px";
-        // YOU CAN CHANGE YOUR NAME HERE               "Good morning Yourname!"
-        document.getElementById('welcome').innerHTML = "Good morning Sempai!";
-    }
-    if(h>=12 && h<=16) {
-        document.getElementById('img').style.backgroundImage = "url('img/day1.jpg')";
-        document.getElementById('img').style.backgroundPositionY = "-250px";
-        // ALSO CHANGE YOUR NAME HERE                  "Good morning Yourname!"
-        document.getElementById('welcome').innerHTML = "Good day Sempai!";
-    }
-    if(h>=17 && h<=19) {
-        document.getElementById('img').style.backgroundImage = "url('img/noon1.jpg')";
-        document.getElementById('img').style.backgroundPositionY = "-150px";
-        // ALSO CHANGE YOUR NAME HERE                  "Good morning Yourname!"
-        document.getElementById('welcome').innerHTML = "Good afternoon Sempai!";
-    }
-    if((h>=20 && h<=23) || (h>=0 && h<=5)){
-        document.getElementById('img').style.backgroundImage = "url('img/night1.jpg')";
-        document.getElementById('img').style.backgroundPositionY = "-240px";
-        // ALSO CHANGE YOUR NAME HERE                  "Good morning Yourname!"
-        document.getElementById('welcome').innerHTML = "Good night Sempai!";
-    }
+function update() {
+  document.getElementById('currentmoney').innerHTML = money;
 }
-function time(){
-    var today = new Date();
+var money = 0;
 
-    var weekday = new Array(7);
-    weekday[0]=  "Sun";
-    weekday[1] = "Mon";
-    weekday[2] = "Tue";
-    weekday[3] = "Wed";
-    weekday[4] = "Thu";
-    weekday[5] = "Fri";
-    weekday[6] = "Sat";
+function timer() {
+  money = money;
+  update()
+}
+setInterval(timer, 1000)
 
-    var day = weekday[today.getDay()];
+function work() {
+  money = money + 1
+  update()
+}
 
-    var d = today.getDate();
-
-    var month = new Array(12);
-    month[0] = "Jan";
-    month[1] = "Feb";
-    month[2] = "Mar";
-    month[3] = "Apr";
-    month[4] = "May";
-    month[5] = "Jun";
-    month[6] = "Jul";
-    month[7] = "Aug";
-    month[8] = "Sep";
-    month[9] = "Oct";
-    month[10] = "Nov";
-    month[11] = "Dec";
-
-    var m = month[today.getUTCMonth()];
-
-    var total = day + " | " + d + " | " + m;
-    console.log(total);
-
-    document.getElementById('currentTime').innerHTML = total;
+function save() {
+  localStorage.setItem("money", money);
+}
+function load() {
+  money = localStorage.getItem("money");
+  money = parseInt(money);
+  update()
 }
