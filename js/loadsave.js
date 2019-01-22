2a26f854df0d9cd22a17b811a6f7c3438a2edff7
Shimeri <mo@shi.moe>
2019-01-23 00:45:24 +0100

    Add files via upload

---
+++ js/loadsave.js
@@ -0,0 +1,58 @@
+//Lock & Load
+setInterval(function(){
+  var save = {
+    "money": stats.money,
+    "xp": stats.xp,
+    "lvl": stats.lvl,
+    "lvlxp": stats.lvlxp,
+    "totalxp": stats.totalxp,
+    "lvlcap": stats.lvlcap,
+    "fasterlaptops": upgrades.fasterlaptops,
+    "costfasterlaptops": upgrades.fasterlaptops.cost,
+    "amountfasterlaptops": upgrades.fasterlaptops.amount,
+    "ownedbedpurple": items.bedpurple.owned,
+    "equipbedpurple": items.bedpurple.equip,
+    "owneddronebat": items.dronebat.owned,
+    "equipdronebat": items.dronebat.equip,
+  };
+  localStorage.setItem("save",JSON.stringify(save));
+}, 10000);
+
+$("#save").click(function(){
+  var save = {
+    "money": stats.money,
+    "xp": stats.xp,
+    "lvl": stats.lvl,
+    "lvlxp": stats.lvlxp,
+    "totalxp": stats.totalxp,
+    "lvlcap": stats.lvlcap,
+    "fasterlaptops": upgrades.fasterlaptops,
+    "costfasterlaptops": upgrades.fasterlaptops.cost,
+    "amountfasterlaptops": upgrades.fasterlaptops.amount,
+    "ownedbedpurple": items.bedpurple.owned,
+    "equipbedpurple": items.bedpurple.equip,
+    "owneddronebat": items.dronebat.owned,
+    "equipdronebat": items.dronebat.equip,
+  };
+  localStorage.setItem("save",JSON.stringify(save));
+});
+
+function loadprogress() {
+  if (localStorage.getItem("save") !== null) {
+    var progress = JSON.parse(localStorage.getItem("save"));
+    stats.money = progress["money"];
+    stats.xp = progress["xp"];
+    stats.lvl = progress["lvl"];
+    stats.lvlxp = progress["lvlxp"];
+    stats.totalxp = progress["totalxp"];
+    stats.lvlcap = progress["lvlcap"];
+    upgrades.fasterlaptops = progress["fasterlaptops"];
+    upgrades.fasterlaptops.cost = progress["costfasterlaptops"];
+    upgrades.fasterlaptops.amount = progress["amountfasterlaptops"];
+    items.bedpurple.owned = progress["ownedbedpurple"];
+    items.bedpurple.equip = progress["equipbedpurple"];
+    items.dronebat.owned = progress["owneddronebat"];
+    items.dronebat.equip = progress["equipdronebat"];
+    update();
+  }
+};
---
+++ js/ui.js
@@ -0,0 +1,63 @@
+//Sounds
+var nya = new Audio('sound/nya.wav');
+var dooropen = new Audio('sound/dooropen.wav');
+var doorclose = new Audio('sound/doorclose.wav');
+$("#mute").click(function() {
+  $(this).addClass("o-50");
+  dooropen.muted = true;
+  doorclose.muted = true;
+});
+
+//Popups
+$("#door").click(function() {
+  $(".megamenu").show();
+  dooropen.play();
+});
+$("#closemegamenu").click(function() {
+  $(".megamenu").hide();
+  doorclose.play();
+});
+$("#wardrobe").click(function() {
+  $(".openwardrobe").show();
+});
+$("#gotoupgradestore").click(function() {
+  $(".upgradestore").show();
+  $(".megamenu").hide();
+});
+$("#gotoshoppingmall").click(function() {
+  $(".shoppingmall").show();
+  $(".megamenu").hide();
+});
+$("#gotolootcrateshop").click(function() {
+  $(".lootcrateshop").show();
+  $(".megamenu").hide();
+});
+$("#closewardrobe").click(function() {
+  $(".openwardrobe").hide();
+});
+$("#exitupgradestore").click(function() {
+  $(".upgradestore").hide();
+});
+$("#exitshoppingmall").click(function() {
+  $(".shoppingmall").hide();
+});
+$("#exitlootcrateshop").click(function() {
+  $(".lootcrateshop").hide();
+});
+
+//Notifications
+$("#closepoor").click(function() {
+  $(".notifypoor").hide();
+});
+
+//Randomize things
+function poor() {
+  var confirm = [
+    "Fuck...",
+    "Shit...",
+    "Damn...",
+    "Fine...",
+    "REEEEE!"
+  ];
+  $("#confirm").text(confirm[Math.floor(Math.random() * confirm.length)]);
+};
